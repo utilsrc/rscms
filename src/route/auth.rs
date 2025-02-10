b22f66e0d445c98160f0dd338aebd2bcb975c930
panhy <panhy@mama.cn>
2025-02-10 18:30:47 +0800

    test deployment

--- src/route/auth.rs
+++ src/route/auth.rs
@@ -3,5 +3,9 @@ use actix_web::web;
 use crate::handler::auth::login;
 
 pub fn auth_routes(cfg: &mut web::ServiceConfig) {
-    cfg.route("/auth/login", web::post().to(login));
+    cfg.service(
+        web::scope("/auth")
+            .route("/login", web::get().to(login)) // correct route: post /auth/token
+            .route("/logout", web::get().to(logout)), // correct route: delete /auth/token
+    );
 }
