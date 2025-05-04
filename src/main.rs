4343371e47d57168acb0dfb6d1034f71559e6d18
panhy <phyuany@foxmail.com>
2025-05-04 12:42:02 +0800

    feature: 完成mongodb数据库的连接测试

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use crate::route::auth::auth_routes;
 use crate::route::index::general_routes;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
-use mongodb::{options::ClientOptions, Client};
+use mongodb::{bson::doc, Client};
 
 #[path = "./handler/mod.rs"]
 mod handler;
@@ -15,26 +15,33 @@ mod route;
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
+    // 读取环境变量
     let host = env::var("RSCMS_SERVER_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("RSCMS_SERVER_PORT").unwrap_or_else(|_| "8080".to_string());
     let port_num = port.parse::<u16>().expect("Invalid port number");
     let mongo_uri = env::var("RSCMS_MONGODB_URI");
+    let db_name = env::var("RSCMS_MONGODB_DB_NAME");
     if mongo_uri.is_err() {
         panic!("MongoDB URL not set!");
     }
+    if db_name.is_err() {
+        panic!("MongoDB database name not set!");
+    }
+    let mongo_uri = mongo_uri.unwrap();
+    let db_name = db_name.unwrap();
 
     // 连接 MongoDB 数据库
-    let mongo_uri_str = mongo_uri.unwrap();
-    let client_options = ClientOptions::parse(&mongo_uri_str)
+    let client = Client::with_uri_str(mongo_uri)
         .await
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    let client = Client::with_options(client_options)
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    let db = client.database("rscms");
+        .unwrap();
+    let database = client.database(db_name.as_str());
+    match database.run_command(doc! {"ping": 1}).await {
+        Ok(_) => println!("✅ Successfully connected to MongoDB database: {}", db_name),
+        Err(e) => panic!("❌ Failed to connect to MongoDB database: {}", e),
+    }
 
     // 共享 MongoDB 数据库实例
-    let db_instance = web::Data::new(db);
-
+    let db_instance = web::Data::new(database);
     let app = move || {
         App::new()
             .configure(general_routes)
@@ -44,13 +51,13 @@ async fn main() -> std::io::Result<()> {
                 web::route().to(|| async { HttpResponse::NotFound().body("404 Not Found") }),
             )
     };
+    
+    // 启动服务器
     let server = HttpServer::new(app)
         .shutdown_timeout(120)
         .bind((host.as_str(), port_num))
         .expect(&format!("Can not bind to port {}:{}", host, port));
-
     println!("Server running at http://{}:{}/", host, port);
-
     server.run().await
 }
 
@@ -60,7 +67,7 @@ mod tests {
 
     #[test]
     fn test_default_password_match() {
-        let passwd = "rscms-admin";
+        let passwd = "rscms";
         let hashed = hash(passwd, DEFAULT_COST).unwrap();
         println!("hashed: {}", hashed);
 
